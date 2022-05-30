ae58dc850f547de8aab27dce060ff485c6883bae
manan mishra <manan.mishra@nagarro.com>
2022-05-30 14:26:11 +0530

    second commit

--- lib/cicd2-stack.ts
+++ lib/cicd2-stack.ts
@@ -4,14 +4,14 @@ import { Construct } from 'constructs';
 
 import{CodePipeline,CodePipelineSource,ShellStep}from'aws-cdk-lib/pipelines';
 import{ManualApprovalStep} from'aws-cdk-lib/pipelines';
-// import{MyPipelineAppStage} from './stage';
+import{MyPipelineAppStage} from './stage';
 
 
 export class Cicd2Stack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new CodePipeline(this,'Pipeline',{
+    const pipeline  = new CodePipeline(this,'Pipeline',{
       pipelineName:'TestPipeline',
       synth:new ShellStep('Synth',{
         input:CodePipelineSource.gitHub('BlackStark2099/sampleCdkToolkit','main'),// Remember to ch
@@ -20,6 +20,18 @@ export class Cicd2Stack extends Stack {
                    'npx cdk synth']
       }),
     });
+
+    const testingstage=pipeline.addStage(new MyPipelineAppStage(this,"test",{
+      env:{account:"637774830294",region:"us-east-1"}
+    }));
+    
+    testingstage.addPost(new ManualApprovalStep('Manual approval before production'));
+
+
+    const prodstage=pipeline.addStage(new MyPipelineAppStage(this,"prod",{
+      env:{account:"637774830294",region:"us-east-1"}
+    }));
+
   }
 }
 
