01035feeffb8c7bcf00a32d2de0a543e128777ff
manan mishra <manan.mishra@nagarro.com>
2022-05-30 13:05:47 +0530

    first commit

--- lib/cicd2-stack.ts
+++ lib/cicd2-stack.ts
@@ -2,15 +2,24 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
+import{CodePipeline,CodePipelineSource,ShellStep}from'aws-cdk-lib/pipelines';
+import{ManualApprovalStep} from'aws-cdk-lib/pipelines';
+// import{MyPipelineAppStage} from './stage';
+
+
 export class Cicd2Stack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'Cicd2Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new CodePipeline(this,'Pipeline',{
+      pipelineName:'TestPipeline',
+      synth:new ShellStep('Synth',{
+        input:CodePipelineSource.gitHub('BlackStark2099/sampleCdkToolkit','main'),// Remember to ch
+        commands:['npm ci',
+                   'npm run build',
+                   'npx cdk synth']
+      }),
+    });
   }
 }
+
