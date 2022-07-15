9594bb6ae216e7e1e27a52dcb11fcf53870fa2aa
manan mishra <manan.mishra@nagarro.com>
2022-07-15 13:08:45 +0530

    sample cdk stack

--- lib/cicd2-stack.ts
+++ lib/cicd2-stack.ts
@@ -3,7 +3,7 @@ import { Construct } from 'constructs';
 import{CodePipeline,CodePipelineSource,ShellStep,CodeBuildOptions}from'aws-cdk-lib/pipelines';
 import{ManualApprovalStep} from'aws-cdk-lib/pipelines';
 import{MyPipelineAppStage} from './stage';
-import * as codepipeline_build from 'aws-cdk-lib/aws-codebuild';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
 
 
 
@@ -11,18 +11,51 @@ export class Cicd2Stack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const invalidateBuildProject = new codebuild.PipelineProject(this, `InvalidateProject`, {
+      
+      cache: codebuild.Cache.local(codebuild.LocalCacheMode.DOCKER_LAYER, codebuild.LocalCacheMode.CUSTOM),
+
+      buildSpec: codebuild.BuildSpec.fromObject({
+        version: '0.2',
+        phases: {
+          build: {
+            commands:['npm ci',
+            'npm run build',
+            'npx cdk synth'],
+          },
+        },
+        cache: {
+          paths: [
+            '/lib/**/*',
+          ],
+        },
+      }),
+    });
+    
+    // Add Cloudfront invalidation permissions to the project
+    // const distributionArn = `arn:aws:cloudfront::${this.account}:distribution/${distribution.distributionId}`;
+    // invalidateBuildProject.addToRolePolicy(new iam.PolicyStatement({
+    //   resources: [distributionArn],
+    //   actions: [
+    //     'cloudfront:CreateInvalidation',
+    //   ],
+    // }));
+    
+    // Create the pipeline (here only the S3 deploy and Invalidate cache build)
+    // const deployBucket = new s3.Bucket(this, 'DeployBucket');
+    // const deployInput = new codepipeline.Artifact();
+    
+
     const pipeline  = new CodePipeline(this,'Pipeline',{
       pipelineName:'TestPipeline',
       synth:new ShellStep('Synth',{
         input:CodePipelineSource.gitHub('BlackStark2099/sampleCdkToolkit','main'),// Remember to ch
-        commands:['npm ci',
-                   'npm run build',
-                   'npx cdk synth'],
+        commands:[''],
       }),
       
       codeBuildDefaults:{
         buildEnvironment: {
-          computeType: codepipeline_build.ComputeType.LARGE
+          computeType: codebuild.ComputeType.LARGE
         },
       }
       
@@ -38,6 +71,8 @@ export class Cicd2Stack extends Stack {
     const prodstage=pipeline.addStage(new MyPipelineAppStage(this,"prod",{
       env:{account:"637774830294",region:"us-east-1"}
     }));
+
+    
 
   }
 }
