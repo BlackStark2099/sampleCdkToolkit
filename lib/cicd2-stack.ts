48a934f02dd1eb085232bec6a5d9059585ad5f65
manan mishra <manan.mishra@nagarro.com>
2022-07-15 11:49:24 +0530

    sample cdk stack

--- lib/cicd2-stack.ts
+++ lib/cicd2-stack.ts
@@ -3,13 +3,46 @@ import { Construct } from 'constructs';
 import{CodePipeline,CodePipelineSource,ShellStep,CodeBuildOptions}from'aws-cdk-lib/pipelines';
 import{ManualApprovalStep} from'aws-cdk-lib/pipelines';
 import{MyPipelineAppStage} from './stage';
-import * as codepipeline_build from 'aws-cdk-lib/aws-codebuild';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
 
 
 
 export class Cicd2Stack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const invalidateBuildProject = new codebuild.PipelineProject(this, `InvalidateProject`, {
+      
+      cache: codebuild.Cache.local(codebuild.LocalCacheMode.DOCKER_LAYER, codebuild.LocalCacheMode.CUSTOM),
+
+      buildSpec: codebuild.BuildSpec.fromObject({
+        version: '0.2',
+        phases: {
+          build: {
+            commands: ['...'],
+          },
+        },
+        cache: {
+          paths: [
+            '/**/*',
+          ],
+        },
+      }),
+    });
+    
+    // Add Cloudfront invalidation permissions to the project
+    // const distributionArn = `arn:aws:cloudfront::${this.account}:distribution/${distribution.distributionId}`;
+    // invalidateBuildProject.addToRolePolicy(new iam.PolicyStatement({
+    //   resources: [distributionArn],
+    //   actions: [
+    //     'cloudfront:CreateInvalidation',
+    //   ],
+    // }));
+    
+    // Create the pipeline (here only the S3 deploy and Invalidate cache build)
+    // const deployBucket = new s3.Bucket(this, 'DeployBucket');
+    // const deployInput = new codepipeline.Artifact();
+    
 
     const pipeline  = new CodePipeline(this,'Pipeline',{
       pipelineName:'TestPipeline',
@@ -22,7 +55,7 @@ export class Cicd2Stack extends Stack {
       
       codeBuildDefaults:{
         buildEnvironment: {
-          computeType: codepipeline_build.ComputeType.LARGE
+          computeType: codebuild.ComputeType.LARGE
         },
       }
       
