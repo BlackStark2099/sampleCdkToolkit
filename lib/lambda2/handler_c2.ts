5445cdc68e631b5e2d11ffa454f9eff8c08cdd4f
manan mishra <manan.mishra@nagarro.com>
2022-07-29 16:01:40 +0530

    added normal stack

--- lib/lambda2/handler_c2.ts
+++ lib/lambda2/handler_c2.ts
@@ -1,7 +1,7 @@
 export async function handler(event:string,context:string){
   console.log('Stage Name is:+process.env.stage');
   return{
-    body:'Hello froma Lambda Function -----------copy 2',
+    body:'Hello froma Lambda Function -----------copy 2   sagdyagsdgashdhgsagdsahg',
     statusCode:200
   };
  }
