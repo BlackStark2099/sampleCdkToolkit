d560baf90c24ed86a53342cd3b28ddd0daa00820
manan mishra <manan.mishra@nagarro.com>
2022-07-29 15:47:10 +0530

    added normal stack

--- lib/lambda-stack.ts
+++ lib/lambda-stack.ts
@@ -12,6 +12,13 @@ export class MyLambdaStack extends cdk.Stack{
         code: Code.fromAsset(path.join(__dirname,"lambda")),
         environment:{"stageName":stageName}
       });
+      
+      new Function(this,'LambdaFunction2',{
+        runtime:Runtime.PYTHON_3_8,
+        handler:'handler.handler',
+        code: Code.fromAsset(path.join(__dirname,"lambda2")),
+        environment:{"stageName":stageName}
+      });
      
     }
 }
